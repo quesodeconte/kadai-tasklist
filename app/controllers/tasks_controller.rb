9f1e41f55c2b0fdb3b4c16d2ccf925a3e1ec4e12
kana.kobayashi <rem.4dyoki@gmail.com>
2020-07-13 12:29:35 +0000

    fix up

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -38,7 +38,7 @@ class TasksController < ApplicationController
     end
     
     def destroy
-        @message.destroy
+        @task.destroy
         
         flash[:success] = 'Task は正常に削除されました'
         redirect_to tasks_url
